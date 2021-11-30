8784cfd1a5bba70acae90f3a45767dd0b2d5c13d
nguyenson257 <nguyenson25701@gmail.com>
2021-11-30 22:33:35 +0700

    Sơn up Tìm Kiếm

--- app/src/main/java/com/myfistapp/sunshine_app/Activity/TrangChu.java
+++ app/src/main/java/com/myfistapp/sunshine_app/Activity/TrangChu.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -14,7 +15,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.myfistapp.sunshine_app.Adapter.CategoryAdapter;
 import com.myfistapp.sunshine_app.Adapter.PhotoViewPagerAdapter;
@@ -38,9 +38,8 @@ public class TrangChu extends AppCompatActivity {
     private RecyclerView.Adapter  adapter;
     private RecyclerView  recyclerViewPopularList, recyclerViewCategoryList;
     private Khachhang khachhang;
-    //Khai báo botomNavigation
-    private BottomNavigationView bottomNavigationView;
 
+    private EditText search_box;
     //Khai báo ImageSlide
     private ViewPager mViewPager;
     private CircleIndicator mCircleIndicator;
@@ -67,7 +66,7 @@ public class TrangChu extends AppCompatActivity {
         if(bundleRecevie!=null){
             khachhang = (Khachhang) bundleRecevie.get("object_user");
         }
-        //Ánh xạ bottomNavigation
+        search_box = findViewById(R.id.search_box);
 
         bottomNavigation();
         //Ánh xạ imageslide
@@ -83,6 +82,18 @@ public class TrangChu extends AppCompatActivity {
         mCircleIndicator.setViewPager(mViewPager);
 
         handler.postDelayed(runnable,3000);
+
+        search_box.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TimKiem.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("object_user",khachhang);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
         mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -102,7 +113,7 @@ public class TrangChu extends AppCompatActivity {
             }
         });
 
-         recyclerViewPopular();
+        recyclerViewPopular();
         recyclerViewCategory();
 
 
@@ -226,16 +237,6 @@ public class TrangChu extends AppCompatActivity {
         int numcol=2;
         recyclerViewPopularList = findViewById(R.id.recycleview_danhsach);
         recyclerViewPopularList.setLayoutManager(new GridLayoutManager(this,2));
-
-
-//        danhsachsanpham.add(new SanPhamDomain("Salad Trứng","img_favorite_food_1","Salad trứng thơm ngon với các nguyên liệu organic phù hợp với chế độ ăn kiêng ít dầu mỡ. Thành phần: xà lách, dầu ô liu, trứng, vừng...","30","328","79000","4.9"));
-//        danhsachsanpham.add(new SanPhamDomain("Mì Ý Rigatoni","img_favorite_food_2","Salad trứng thơm ngon với các nguyên liệu organic phù hợp với chế độ ăn kiêng ít dầu mỡ. Thành phần: xà lách, dầu ô liu, trứng, vừng...","20","111","98000","4.0"));
-//        danhsachsanpham.add(new SanPhamDomain("Chicken Hamburger","img_favorite_food_3","Salad trứng thơm ngon với các nguyên liệu organic phù hợp với chế độ ăn kiêng ít dầu mỡ. Thành phần: xà lách, dầu ô liu, trứng, vừng...","15","33","45000","4.9"));
-//        danhsachsanpham.add(new SanPhamDomain("Salad Cá Hồi","img_favorite_food_4","Salad trứng thơm ngon với các nguyên liệu organic phù hợp với chế độ ăn kiêng ít dầu mỡ. Thành phần: xà lách, dầu ô liu, trứng, vừng...","35","123","23000","5"));
-//        danhsachsanpham.add(new SanPhamDomain("Bánh Muffin","img_favorite_food_5","Salad trứng thơm ngon với các nguyên liệu organic phù hợp với chế độ ăn kiêng ít dầu mỡ. Thành phần: xà lách, dầu ô liu, trứng, vừng...","25","532","41000","3.9"));
-//        danhsachsanpham.add(new SanPhamDomain("Nước ép dưa hấu","img_favorite_food_6","Salad trứng thơm ngon với các nguyên liệu organic phù hợp với chế độ ăn kiêng ít dầu mỡ. Thành phần: xà lách, dầu ô liu, trứng, vừng...","10","123","12000","4.9"));
-//        danhsachsanpham.add(new SanPhamDomain("Trái cây mix","img_favorite_food_7","Salad trứng thơm ngon với các nguyên liệu organic phù hợp với chế độ ăn kiêng ít dầu mỡ. Thành phần: xà lách, dầu ô liu, trứng, vừng...","30","112","44000","4.9"));
-//        danhsachsanpham.add(new SanPhamDomain("Nghêu sốt bơ tỏi","img_favorite_food_8","Salad trứng thơm ngon với các nguyên liệu organic phù hợp với chế độ ăn kiêng ít dầu mỡ. Thành phần: xà lách, dầu ô liu, trứng, vừng...","20","423","22000","5"));
 
         ApiService.apiService.showitem().enqueue(new Callback<ArrayList<SanPhamDomain>>() {
             @Override
